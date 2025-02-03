e5e1d910978e54c34f431d0850acefe8f79ad7a8
Vito <vit_oo@sjtu.edu.cn>
2025-02-03 23:58:25 +0100

    Cool hero

--- Saltedfish.js
+++ Saltedfish.js
@@ -159,7 +159,7 @@ function initHero() {
   // 初始化血条
   const heroHPBar = document.getElementById('heroHPBar');
   heroHPBar.style.width = '50px'; // 初始血条宽度
-  heroHPBar.style.left = `${hero.x}px`; // 血条位置与主角一致
+  heroHPBar.style.left = `${hero.x + 80}px`; // 血条位置与主角一致
   heroHPBar.style.top = `${hero.y - 20}px`; // 血条位于主角上方
 }
 
@@ -264,8 +264,8 @@ function spawnBullet() {
 
     bulletDiv.style.backgroundImage = `url('${bulletimg}')`;
   // 子弹初始位置：主角正中上方
-  const bulletX = hero.x;
-  const bulletY = hero.y - 15;
+  const bulletX = hero.x + 50;
+  const bulletY = hero.y + 10;
   let effectiveDamage = bulletAttack;
     if (weapontype === 2) {
         effectiveDamage = bulletAttack * 3; // 爆炸弹伤害=300%
@@ -881,8 +881,8 @@ function updateHeroHPBar() {
   const heroHPBar = document.getElementById('heroHPBar');
   const hpPercentage = (playerHP / playerHPinitial) * 10; // 计算血量百分比
   heroHPBar.style.width = `${hpPercentage}%`; // 根据血量百分比调整血条宽度
-  heroHPBar.style.left = `${hero.x}px`; // 血条位置与主角一致
-  heroHPBar.style.top = `${hero.y - 20}px`; // 血条位于主角上方
+  heroHPBar.style.left = `${hero.x+ 57}px`; // 血条位置与主角一致
+  heroHPBar.style.top = `${hero.y + 50}px`; // 血条位于主角上方
 }
 
 /********************
@@ -1454,8 +1454,8 @@ function startGame() {
   // 重置游戏状态
   isGameOver = false;
   hero.isAlive = true;
-  hero.x = containerWidth / 2 - 25;
-  hero.y = containerHeight - 70;
+  hero.x = containerWidth / 2 - 70;
+  hero.y = containerHeight - 150;
 
   bullets.length = 0;
   monsters.length = 0;
@@ -1497,4 +1497,6 @@ document.getElementById('startButton').addEventListener('click', (event) => {
   document.getElementById('mainMenu').style.display = 'none'; // 隐藏主菜单
   playOpeningAnimation(); // 播放动画
 });
+
+
 
